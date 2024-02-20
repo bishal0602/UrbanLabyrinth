fc89c98ec3912356ecc59976c05d92d0f2aa8a27
bishal0602 <bishalpanta60@gmail.com>
2024-02-21 00:53:30 +0545

    Updated map with elevation and accessories

--- src/scripts/main.gd
+++ src/scripts/main.gd
@@ -7,9 +7,10 @@ func _ready():
 
 func setup():
 		# Wait for the first physics frame so the NavigationServer can sync.
+		player.set_movement_target(target.global_position)
 		await get_tree().physics_frame
 		set_physics_process(true)
 
-func _physics_process(delta):
-		#get_tree().call_group("enemies", "set_movement_target", player.global_transform.origin)
-		player.set_movement_target(target.global_position)
+#func _physics_process(delta):
+		##get_tree().call_group("enemies", "set_movement_target", player.global_transform.origin)
+		#player.set_movement_target(target.global_position)
